04e6533756fece0f3974622f8c5a62d86e290613
yuec621 <yuec621@outlook.com>
2020-10-10 10:31:35 +0800

    'pass语句'

---
+++ demo_pass.py
@@ -0,0 +1,16 @@
+# 哔站python学习教程
+# 听讲人 cy
+# 开发时间：2020/10/10 0010 10:24
+'''answer=input('您是会员吗?y/n')
+#判断是否是会员
+if answer=='y':
+    pass
+else:
+    pass
+'''
+
+age=int(input('请输入您的年龄:'))
+if age:
+    print(age)
+else:
+    print('年龄为:',age)
